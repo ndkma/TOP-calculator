9e6b763ee3229c40080e9b4f56e483e0aef206eb
ndkma <nicowork13@gmail.com>
2022-06-27 14:04:23 +0800

    Add functionality to the operators and equal-sign
    Added basic functionality to the calculator. Simple inputs in logical
    order will work. Eg. "5 + 5 =" will output 10. Still needs failsafes.

--- script.js
+++ script.js
@@ -1,35 +1,94 @@
-let calcDisplay = document.querySelector(".display-current");
+let calcDisplayCurrent = document.querySelector(".display-current");
+let calcDisplayPrevious = document.querySelector(".display-previous");
 
 const numBtn = document.querySelectorAll(".button");
 numBtn.forEach(function(pressedButton){
     pressedButton.addEventListener('click', () => {
-        if (calcDisplay.textContent.length < 10) {
-        return calcDisplay.textContent = calcDisplay.textContent + pressedButton.innerText
+        if (calcDisplayCurrent.textContent.length < 10) {
+        return calcDisplayCurrent.textContent = calcDisplayCurrent.textContent + pressedButton.innerText
         }
     });
 });
 
 const periodBtn = document.querySelector(".period");
 periodBtn.addEventListener('click', () => {
-    if ((calcDisplay.textContent.length < 10) && (calcDisplay.textContent.includes('.') == false) &&
-    (calcDisplay.textContent != "")) {
+    if ((calcDisplayCurrent.textContent.length < 10) && (calcDisplayCurrent.textContent.includes('.') == false) &&
+    (calcDisplayCurrent.textContent != "")) {
         
-        return calcDisplay.textContent = calcDisplay.textContent + periodBtn.innerText;
+        return calcDisplayCurrent.textContent = calcDisplayCurrent.textContent + periodBtn.innerText;
     }
-});
-
-const clearBtn = document.querySelector(".clear");
-clearBtn.addEventListener('click', () => {
-    return calcDisplay.textContent = "";
 });
 
 const backBtn = document.querySelector(".back");
 backBtn.addEventListener('click', () => {
-    return calcDisplay.textContent = calcDisplay.textContent.slice(0, (calcDisplay.textContent.length-1));
+    return calcDisplayCurrent.textContent = calcDisplayCurrent.textContent.slice(0, (calcDisplayCurrent.textContent.length-1));
 
 });
 
-//------------------------------LETS TRY----------------------------------//
+
+let num1 = "";
+let num2 = "";
+let operator = "";
+let answer = "";
+
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener('click', () => {
+    num1 = "";
+    num2 = "";
+    operator = "";
+    answer = "";
+    calcDisplayCurrent.textContent = "";
+    calcDisplayPrevious.textContent = "";
+});
+
+const addBtn = document.querySelector(".add");
+addBtn.addEventListener('click', () => {
+   num1 = Number(calcDisplayCurrent.textContent)
+   calcDisplayPrevious.textContent = num1
+   calcDisplayCurrent.textContent = ""
+   operator = "+"
+});
+
+const subtractBtn = document.querySelector(".subtract");
+subtractBtn.addEventListener('click', () => {
+   num1 = Number(calcDisplayCurrent.textContent)
+   calcDisplayPrevious.textContent = num1
+   calcDisplayCurrent.textContent = ""
+   operator = "-"
+});
+
+const multiplyBtn = document.querySelector(".multiply");
+multiplyBtn.addEventListener('click', () => {
+   num1 = Number(calcDisplayCurrent.textContent)
+   calcDisplayPrevious.textContent = num1
+   calcDisplayCurrent.textContent = ""
+   operator = "*"
+});
+
+const divideBtn = document.querySelector(".divide");
+divideBtn.addEventListener('click', () => {
+   num1 = Number(calcDisplayCurrent.textContent)
+   calcDisplayPrevious.textContent = num1
+   calcDisplayCurrent.textContent = ""
+   operator = "/"
+});
 
 
+const equalBtn = document.querySelector(".equal-sign");
+equalBtn.addEventListener('click', () => {
+    num2 = Number(calcDisplayCurrent.textContent)
+    if (operator == "+") {
+        answer = num1 + num2
+        calcDisplayCurrent.textContent = answer
+    } else if (operator == "-") {
+        answer = num1 - num2
+        calcDisplayCurrent.textContent = answer
+    } else if (operator == "*") {
+        answer = num1 * num2
+        calcDisplayCurrent.textContent = answer
+    } else if (operator == "/") {
+        answer = num1 / num2
+        calcDisplayCurrent.textContent = answer
+    }
+})
 
